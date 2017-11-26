e89a7ad18ad8874b8fe546a209ecc772ed7a0035
stopuz <sinantopuz@gmail.com>
2017-11-26 13:48:21 -0500

    initial commit

---
+++ Funtions.py
@@ -0,0 +1,16 @@
+import sys
+
+k = [1, 3, 6]
+
+
+def modify(k):
+    k.append(39)
+    print(k)
+
+
+def banner(message, border='-'):
+    line = border * len(message)
+    print(line)
+    print(message)
+    print(line)
+
